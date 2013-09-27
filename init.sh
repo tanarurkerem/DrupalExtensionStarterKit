1bc93b2abf10a1a15d4a96f92914305690c36bd7
István Palócz <istvan@palocz.hu>
2013-09-27 06:52:03 +0200

    This script not use variables

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 curl -sS https://getcomposer.org/installer | php
 php composer.phar install
-if [ ! -d "$DIRECTORY" ]; then
+if [ ! -d "bin/selenium" ]; then
   mkdir -p bin/selenium
   cd bin/selenium
   wget http://selenium.googlecode.com/files/selenium-server-standalone-2.35.0.jar
